408fa8b8169f04fc87508ce800dd9fc7100d0ce6
Khan Faiz <khan.faiz.ayub@gmail.com>
2023-07-20 11:27:02 +0530

    function

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 "use strict";
+
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
 if (passTest) hasDriversLicense = true;
 if (hasDriversLicense) console.log("I can drive 🚗");
+*/
+
+// ************************    FUNCTIONS      ************************
+/*
+function logger() {
+  console.log("My name is Faiz");
+}
+logger();
+
+function carSpeed(gear, speed) {
+  const car = `car is on ${gear} gear and speed of car is ${speed} kph.`;
+  return car;
+}
+// const speedCheck=carSpeed(6, 285);
+console.log(carSpeed(6, 285));
+
+
+function swim(distance, time) {
+  const swimmer = `faiz has completed ${distance} km in time ${time} minutes.`;
+  return swimmer;
+}
+const faiz = swim(5, 59);
+console.log(faiz);
+*?
