ed8b1eb0387fda10f4577bf0f0d4407f042aa60d
Khan Faiz <khan.faiz.ayub@gmail.com>
2023-08-01 17:37:53 +0530

    challenge#04

--- script.js
+++ script.js
@@ -495,3 +495,32 @@ while (dice !== 6) {
 */
 
 // ************************ CHALLENGE #04   ************************
+/*
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+const bill = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const total = [];
+
+for (let i = 0; i < bill.length; i++) {
+  const tip = calcTip(bill[i]);
+  tips.push(tip);
+  total.push(tip + bill[i]);
+}
+
+console.log(bill, tips, total);
+
+const calcAverage = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    // sum = sum+arr[i];
+    sum += arr[i];
+  }
+  return sum / arr.length;
+};
+console.log(calcAverage([2, 3, 4]));
+console.log(calcAverage(total));
+console.log(calcAverage(tips));
+*/
